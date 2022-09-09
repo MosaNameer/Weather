735e2f97fa9c61e7a2a379473841387a15450d91
Mosa Nameer <mosa.mn777@gmail.com>
2022-09-09 22:48:50 +0300

    add

---
+++ src/runtime/composables/getWeather.ts
@@ -0,0 +1,24 @@
+import { ref } from "vue";
+
+const getWeather = () => {
+  const weather = ref('');
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      //
+      let data = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=28f543541a4bc931013418a9a0e6d673");
+      if (!data.ok) {
+        throw Error("no data here");
+      } else {
+        weather.value = await data.json();
+      }
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+  return { weather, error, load };
+};
+export default getWeather;
