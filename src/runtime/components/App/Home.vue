ec377570e9702db074feec8491d374e079074dbf
Mosa Nameer <mosa.mn777@gmail.com>
2022-09-10 21:08:14 +0300

    add

--- src/runtime/components/App/Home.vue
+++ src/runtime/components/App/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="absolute top-0 mt-30" v-if="city">
+  <div class=" h-full w-full " v-if="city">
+    <img class="h-full w-full" :src="background">
     <!-- <img class="h-full w-full" :src="background"> -->
-    <div class="w-screen top-0 ">
+    <div class="absolute top-0 mt-30 w-screen top-0 ">
       <div class="flex justify-between  h-60">
         <div class="ml-50">
           <div class="text-center">
@@ -20,25 +21,25 @@
           </div>
         </div>
 
-          <div>
-            <div class="grid grid-cols-12 w-125 h-72 shadow-xl rounded-lg border p-3 mr-50 bg-black bg-opacity-60	">
-              <div class="col-span-5 ml-4">
-                <h2><a href="" class="text-white text-3xl">  {{ city?.name}} / {{ city?.sys.country}}  </a></h2>
-                <div class="flex flex-col mt-6">
-                  <span class="text-white text-7xl">{{ city?.main.temp}}°</span>
-                  <span class="text-gray-3 text-lg"> {{ city?.weather[0].description}} </span>
-                  <span class="text-gray-3 text-lg"> wind speed {{ city.wind.speed}}</span>
-                  <span class="text-gray-3 text-lg"> MIN - {{ city.main.temp_min }} </span>
-                  <span class="text-gray-3 text-lg"> MAX - {{ city.main.temp_max }} </span>
+        <div>
+          <div class="grid grid-cols-12 w-125 h-72 shadow-xl rounded-lg border p-3 mr-50 bg-black bg-opacity-60	">
+            <div class="col-span-5 ml-4">
+              <h2><a href="" class="text-white text-3xl"> {{ city?.name}} / {{ city?.sys.country}} </a></h2>
+              <div class="flex flex-col mt-6">
+                <span class="text-white text-7xl">{{ city?.main.temp}}°</span>
+                <span class="text-gray-3 text-lg"> {{ city?.weather[0].description}} </span>
+                <span class="text-gray-3 text-lg"> wind speed {{ city.wind.speed}}</span>
+                <span class="text-gray-3 text-lg"> MIN - {{ city.main.temp_min }} </span>
+                <span class="text-gray-3 text-lg"> MAX - {{ city.main.temp_max }} </span>
 
-                </div>
-              </div>
-              <div class="col-span-7">
-                <!-- get the icon -->
-                <img :src="`https://openweathermap.org/img/wn/${city?.weather[0].icon}@4x.png`" class="mt-6 w-72">
               </div>
             </div>
+            <div class="col-span-7">
+              <!-- get the icon -->
+              <img :src="`https://openweathermap.org/img/wn/${city?.weather[0].icon}@4x.png`" class="mt-6 w-72">
+            </div>
           </div>
+        </div>
       </div>
     </div>
   </div>
@@ -77,12 +78,12 @@ const { data: city, error } = useAsyncData("city", async () => {
     cookie.value = search.value;
 
     const temp = response.main.temp;
-    if (temp <= -5) {
+    if (temp <= -10) {
       background.value = "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
     } else if (temp > -10 && temp <= 5) {
-      background.value = "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      background.value = "https://cdn.pixabay.com/photo/2021/01/09/17/22/forest-5903010_960_720.jpg";
     } else if (temp > 5 && temp <= 20) {
-      background.value = "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      background.value = "https://cdn.pixabay.com/photo/2017/08/01/01/17/beach-2562563_960_720.jpg";
     } else {
       background.value = "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
     }
@@ -102,39 +103,35 @@ today = today.toLocaleDateString("en-US", {
   month: "long",
   day: "numeric",
 });
-// get time with seconds with
-let time = new Date()
-time = time.toLocaleTimeString("en-US", {
-  hour: "numeric",
-  minute: "numeric",
-  //second: "numeric",
-  hour12: true,
-});
+//time
+const time = ref()
+setInterval(() => {
+  time.value = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    //second: "numeric",
+    hour12: true,
+  });
+}, 1000);
+
+//get day using interval
+const day = ref()
+setInterval(() => {
+  day.value = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+}, 1000);
 
 
-//get day
-let day = new Date()
-day = day.toLocaleDateString("en-US", {
-  weekday: "long",
-});
+//get month and day using interval
+const month = ref()
+setInterval(() => {
+  month.value = new Date().toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+  });
+}, 1000);
 
-//get month and day
-let month = new Date()
-month = month.toLocaleDateString("en-US", {
-  month: "long",
-  day: "numeric",
-});
-
-// const classes = computed(() => {
-//     switch (props.size) {
-//         case '1':
-//             return 'text-3xl'
-//         case '2':
-//             return 'text-2xl'
-//         case '3':
-//             return 'text-xl'
-//     }
-// })
 
 // const { weather, load } = getWeather(search.value);
 // load();
--- src/runtime/components/Weather/Main.vue
+++ src/runtime/components/Weather/Main.vue
@@ -1,9 +1,7 @@
 <template >
   <!-- Application -->
   <div dir="ltr">
-
     <div class="h-screen md:relative items-center md:overflow-hidden">
-      <img class="h-full w-full" src="https://cdn.pixabay.com/photo/2019/08/21/19/26/rain-4421893_960_720.jpg">
       <AppHome />
     </div>
   </div>
